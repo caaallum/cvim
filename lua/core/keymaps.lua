d50aea32073e36c70f3ee3d6328382c8e37f298b
caaallum <callum_speirs@protonmail.com>
2022-12-15 12:56:27 +0000

    Update tree keybinds

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -33,7 +33,7 @@ vim.keymap.set('n', '>b', '<cmd>BufferLineMoveNext<cr>')
 vim.keymap.set('n', '<b', '<cmd>BufferLineMovePrev<cr>')
 
 -- Nvim Tree
-vim.keymap.set('n', '<c-n>', '<cmd>NvimTreeFindFileToggle<cr>')
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeFindFileToggle<cr>')
 
 -- Resizing splits
 vim.keymap.set('n', '<C-Left>', require('smart-splits').resize_left)

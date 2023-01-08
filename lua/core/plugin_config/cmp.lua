af62fd88233eb9cbb79225719416f3c826b66d51
caaallum <callum_speirs@protonmail.com>
2023-01-08 21:25:06 +0000

    Update plugins

--- lua/core/plugin_config/cmp.lua
+++ lua/core/plugin_config/cmp.lua
@@ -108,3 +108,7 @@ cmp.setup({
         { name = 'path', max_item_count = 10 }
     }),
 })
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
+                 {update_in_insert = true})
--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -1,7 +1,7 @@
 require 'nvim-treesitter.configs'.setup {
   ensure_installed = { "c", "cpp", "cmake", "bash", "lua", "vim", "make", "markdown", "php", "m68k", "python" },
 
-  sync_install = false,
+  sync_install = true,
   auto_install = true,
   highlight = {
       enable = true,
